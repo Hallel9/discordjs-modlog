05c2e569c01b2b3be46fbb646a6a9f1a17c9dceb
hallel9 <47569515+Hallel9@users.noreply.github.com>
2021-06-28 04:17:32 +0000

    New Commit

--- test/index.js
+++ test/index.js
@@ -6,7 +6,20 @@ client.on('ready', () => {
 	console.log('Ready!');
 })
 
+client.mod.setURL('mongodb://localhost:27017/mod-log');
+
 client.on('message', async (message) => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
-})
+
+	if (command === 'set-mod-log') {
+		const channel = message.mentions.channels.first();
+
+		if (!channel) return message.channel.send('Please specify a channel.');
+
+		await client.mod.setModLog(message.guild.id, channel.id);
+		message.channel.send(`Channel has successfully been set to ${channel}`);
+ 	}
+});
+
+client.login('token');
